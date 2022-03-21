49f9ec1bc1be019aa5d0dd7ed11dbe37b4d79825
gregory <aanonimov117@gmail.com>
2022-03-21 22:17:35 +0300

    another try to fix db

--- postgres/20-create-schemas.sh
+++ postgres/20-create-schemas.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/13/main/pg_hba.conf
-echo "listen_addresses='*'" >> /etc/postgresql/13/main/postgresql.conf
+# echo "host all  all    0.0.0.0/0  md5" >> /etc/postgresql/13/main/pg_hba.conf
+# echo "listen_addresses='*'" >> /etc/postgresql/13/main/postgresql.conf
 export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=test
-psql -U program -d art -f "$SCRIPT_PATH/schemes/schema.sql"
+psql -U postgres -d postgres -f "$SCRIPT_PATH/schemes/schema.sql"
