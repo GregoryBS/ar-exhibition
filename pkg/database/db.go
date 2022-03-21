06a1fd7ac293abad3919c0e02d2521d31fa4b167
gregory <aanonimov117@gmail.com>
2022-03-21 10:33:49 +0300

    connection to db

---
+++ pkg/database/db.go
@@ -0,0 +1,36 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgconn"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type DBInterface interface {
+	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
+	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
+	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
+	Close()
+}
+
+type DBManager struct {
+	Pool DBInterface
+}
+
+func Connect() *DBManager {
+	connString := os.Getenv("DATABASE_URL")
+	pool, err := pgxpool.Connect(context.Background(), connString)
+	if err != nil {
+		return nil
+	}
+	fmt.Println("Successful connection to postgres")
+	return &DBManager{Pool: pool}
+}
+
+func Disconnect(db *DBManager) {
+	db.Pool.Close()
+}
