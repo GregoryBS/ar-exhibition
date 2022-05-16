85f150e1d32b3363b8b7a7846412ddfdf411e3c6
gregory <aanonimov117@gmail.com>
2022-05-16 19:26:17 +0300

    add table for statistic data, delete unused files

--- go.mod
+++ go.mod
@@ -35,3 +35,5 @@ require (
 	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
+
+require github.com/go-redis/redis v6.15.9+incompatible // indirect
--- postgres/20-create-schemas.sql
+++ postgres/20-create-schemas.sql
@@ -1,3 +1,4 @@
+
 create table museum
 (
     id             serial primary key,
@@ -12,8 +13,8 @@ create table museum
     mus_show       boolean not null default false
 );
 
-insert into museum(name, info, description, image, image_height, image_width) 
-values('Museum', '{"Страна":"Россия","Город":"Москва","Адрес":"2-я Бауманская, 5","Год":"2022","Руководитель":"Гордин Михаил"}'::json, 'Most beautiful museum in Moscow', 'default.jpg', 835, 600);
+insert into museum(name, info, description, image, image_height, image_width, mus_show) 
+values('Museum', '{"Страна":"Россия","Город":"Москва","Адрес":"2-я Бауманская, 5","Год":"2022","Руководитель":"Гордин Михаил"}'::json, 'Most beautiful museum in Moscow', 'default.jpg', 835, 600, true);
 
 create table exhibition
 (
@@ -31,8 +32,8 @@ create table exhibition
     mus_show     boolean not null default false
 );
 
-insert into exhibition(museum_id, name, description, info, image, image_height, image_width) 
-values(1, 'Exhibition', 'Most beautiful exhibition in Moscow', '{"Начало":"2022-10-08","Конец":"2022-10-11"}'::json, 'default.jpg', 835, 600);
+insert into exhibition(museum_id, name, description, info, image, image_height, image_width, exh_show, mus_show) 
+values(1, 'Exhibition', 'Most beautiful exhibition in Moscow', '{"Начало":"2022-05-08","Конец":"2022-10-11"}'::json, 'default.jpg', 835, 600, true, true);
 
 create table picture
 (
@@ -46,13 +47,15 @@ create table picture
     width       real not null default 0,
     popular     bigint not null default 0,
     user_id     int not null default 0,
+    video       text not null default '',
+    video_size  text not null default '',
     pic_show    boolean not null default false,
     exh_show    boolean[] not null default '{}',
     mus_show    boolean not null default false
 );
 
-insert into picture(exh_id, name, image, description, info, height, width) 
-values(1, 'Cat', 'default.jpg,notfound.jpg', 'First picture in the app', '{"Автор":"Человек","Год":"2021","Техника":"Компьютерная графика","Размер":"3 х 2"}'::json, 835, 600);
+insert into picture(exh_id, name, image, description, info, height, width, pic_show, exh_show, mus_show) 
+values('{1}', 'Cat', 'default.jpg,notfound.jpg', 'First picture in the app', '{"Автор":"Человек","Год":"2021","Техника":"Компьютерная графика","Размер":"3 х 2"}'::json, 835, 600, true, '{1}', true);
 
 create table users 
 (
@@ -60,4 +63,15 @@ create table users
     login    text unique not null,
     password bytea not null,
     admin    boolean not null default false
+);
+
+create table stats 
+(
+    id      bigserial primary key,
+    port    int not null,
+    method  text not null,
+    status  int not null,
+    urls    text not null,
+    reqtime timestamp default now()::timestamp,
+    perform int
 );
